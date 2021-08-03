8232088503ac64b320352f8d29aaffa27db37b21
michael preston <mpreston@ucsd.edu>
2021-08-02 17:10:35 -0700

    define all settings/params/vars first

--- code/sim_field/lfp_sims.py
+++ code/sim_field/lfp_sims.py
@@ -18,7 +18,19 @@ from funcs import *
 ###################################################################################################
 
 ## SETTINGS
-fs = 1000
+
+# simulation parameters
+FS = 1000 # sampling frequency
+EI_RATIO = np.linspace(2, 6, 21)
+EI_RATIO_1C = 4 # must be in EI_RATIO
+EI_RATIO_1E = [2, 6] # must be in EI_RATIO
+N_SIMS = 5 # number f replications for each E:I ratio
+
+# spectral parameterization settings
+F_RANGE_FIT = [30, 50] # frequency range for slope fit (for batch of EI ratios)
+F_RANGE_CENTER = np.arange(20, 160.1, 5) # center of freq range (for batch fitting)
+F_RANGE_WIDTH = 20 # width of freq range (for batch fitting)
+
 
 ###################################################################################################
 ###################################################################################################
@@ -26,7 +38,7 @@ fs = 1000
 def main():
 
     # Initialize any output variables to save out
-    LFP_E, LFP_I, t = sim_field(4)
+    LFP_E, LFP_I, t = sim_field(EI_RATIO_1C)
     LFP = LFP_E + LFP_I
 
     fig1, axes = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
@@ -41,13 +53,13 @@ def main():
 
     # PSD
     # PSD Excitatory
-    freq_e, psd_e = compute_spectrum(LFP_E, fs, method='welch', avg_type='median', nperseg=fs, noverlap=int(fs/2))
+    freq_e, psd_e = compute_spectrum(LFP_E, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
 
     # PSD Inhibitory
-    freq_i, psd_i = compute_spectrum(LFP_I, fs, method='welch', avg_type='median', nperseg=fs, noverlap=int(fs/2))
+    freq_i, psd_i = compute_spectrum(LFP_I, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
 
     # PSD LFP
-    freq_lfp, psd_lfp = compute_spectrum(LFP, fs, method='welch', avg_type='median', nperseg=fs, noverlap=int(fs/2))
+    freq_lfp, psd_lfp = compute_spectrum(LFP, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
 
     # Plot the power spectra
     plot_power_spectra([freq_lfp[:200], freq_e[:200], freq_i[:200]],
@@ -61,15 +73,14 @@ def main():
     
 
     #############################
-    EI_ratios= np.arange(2, 6.01, 0.2)
-    center_freqs = np.arange(20, 160.1, 5)
-    PSDs, freq_lfp = batchsim_PSDs(EI_ratios= np.arange(2, 6.01, 0.2), num_trs = 5)
-    slopes = batchfit_PSDs(PSDs, freq_lfp, EI_ratios = EI_ratios, num_trs = 5, freq_range = [30, 50])
-    rhos = batchcorr_PSDs(PSDs, freq_lfp, EI_ratios = EI_ratios, center_freqs = center_freqs, 
-                    win_len = 20, num_trs = 5)
+
+    PSDs, freq_lfp = batchsim_PSDs(EI_ratios=EI_RATIO, num_trs=N_SIMS)
+    slopes = batchfit_PSDs(PSDs, freq_lfp, EI_ratios = EI_RATIO, num_trs=N_SIMS, freq_range=F_RANGE_FIT)
+    rhos = batchcorr_PSDs(PSDs, freq_lfp, EI_ratios = EI_RATIO, center_freqs=F_RANGE_CENTER, 
+                    win_len=F_RANGE_WIDTH, num_trs=N_SIMS)
 
     df = pd.DataFrame(slopes, columns = ['Trial1','Trial2','Trial3','Trial4','Trial5'])
-    df['EIRatio'] = 1./EI_ratios
+    df['EIRatio'] = 1./EI_RATIO
     df_plot = df.melt('EIRatio')
     ax = sns.lineplot(data=df_plot, x = 'EIRatio', y = 'value', marker='o', color = 'black')
     ax.set_xscale('log', base=2)
@@ -80,7 +91,7 @@ def main():
     fig4.savefig('1F.png')
     ax.clear()
     df2 = pd.DataFrame(rhos, columns = ['Trial1','Trial2','Trial3','Trial4','Trial5'])
-    df2['CenterFreq'] = center_freqs
+    df2['CenterFreq'] = F_RANGE_CENTER
     df2_plot = df2.melt('CenterFreq')
     ax2 = sns.lineplot(data=df2_plot, x = 'CenterFreq', y = 'value', marker='o', color = 'black')
     ax2.set_xlabel('Fit Center Frequency (+-10 Hz)')
@@ -89,14 +100,14 @@ def main():
     fig5 = ax2.get_figure()
     fig5.savefig('1G.png')
 
-    LFP_E, LFP_I, t = sim_field(2) # EI ratio = 1 : 2
+    LFP_E, LFP_I, t = sim_field(EI_RATIO_1E[0]) # EI ratio = 1 : 2
     LFP2 = LFP_E + LFP_I
-    LFP_E, LFP_I, _ = sim_field(6) # EI ratio = 1 : 6
+    LFP_E, LFP_I, _ = sim_field(EI_RATIO_1E[1]) # EI ratio = 1 : 6
     LFP6 = LFP_E + LFP_I
     # PSD Excitatory
-    freq_2, psd_2 = compute_spectrum(LFP2, fs, method='welch', avg_type='median', nperseg=fs, noverlap=int(fs/2))
+    freq_2, psd_2 = compute_spectrum(LFP2, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
     # PSD Inhibitory
-    freq_6, psd_6 = compute_spectrum(LFP6, fs, method='welch', avg_type='median', nperseg=fs, noverlap=int(fs/2))
+    freq_6, psd_6 = compute_spectrum(LFP6, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
     # Plot the power spectra
     plot_power_spectra([freq_2[:1000], freq_6[:1000]],
                     [psd_2[:1000], psd_6[:1000]],
