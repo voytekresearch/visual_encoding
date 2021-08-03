ecc651c0469daa3d1b25d72ffc2271d05fb36d64
michael preston <mpreston@ucsd.edu>
2021-08-02 18:19:51 -0700

    improve readability of lfp_sims

--- code/sim_field/lfp_sims.py
+++ code/sim_field/lfp_sims.py
@@ -12,7 +12,7 @@ from neurodsp.spectral import compute_spectrum
 from neurodsp.plts.spectral import plot_power_spectra
 import sys
 sys.path.append('./')
-from funcs import *
+from funcs import sim_field, batchsim_PSDs, batchfit_PSDs, batchcorr_PSDs
 
 ###################################################################################################
 ###################################################################################################
@@ -53,25 +53,30 @@ def simulate_lfp():
     LFP = LFP_E + LFP_I
 
     # Compute power spectrum of LFP and E/I conductance
-    _, psd_e = compute_spectrum(LFP_E, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
-    _, psd_i = compute_spectrum(LFP_I, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
-    freq, psd_lfp = compute_spectrum(LFP, FS, method='welch', avg_type='median', nperseg=FS, noverlap=int(FS/2))
+    _, psd_e = compute_spectrum(LFP_E, FS, method='welch', avg_type='median', 
+                                nperseg=FS, noverlap=int(FS/2))
+    _, psd_i = compute_spectrum(LFP_I, FS, method='welch', avg_type='median', 
+                                nperseg=FS, noverlap=int(FS/2))
+    freq, psd = compute_spectrum(LFP, FS, method='welch', avg_type='median', 
+                                     nperseg=FS, noverlap=int(FS/2))
 
     # Figure 1, C. 
     # Plot time-series 
     samples_2_plot = int(np.floor(FS)) # ~1 seconds
     fig_1c, axes = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
-    ax = sns.lineplot(x=t[:samples_2_plot], y=LFP_E[:samples_2_plot], ax = axes[0])
-    sns.lineplot(x=t[:samples_2_plot], y=LFP_I[:samples_2_plot], ax = axes[0])
+    ax = sns.lineplot(x=t[:samples_2_plot], y=LFP_E[:samples_2_plot], 
+                      ax=axes[0])
+    sns.lineplot(x=t[:samples_2_plot], y=LFP_I[:samples_2_plot], ax=axes[0])
     ax.legend(labels=["Excitatory","Inhibitory"])
 
-    ax = sns.lineplot(x=t[:samples_2_plot], y=LFP[:samples_2_plot], ax=axes[1], color='black')
+    ax = sns.lineplot(x=t[:samples_2_plot], y=LFP[:samples_2_plot], ax=axes[1], 
+                      color='black')
     ax.legend(labels=["LFP"])
     fig_1c.savefig('1C.png')
 
     # Plot Figure 1, D. 
     # Plot power spectra
-    plot_power_spectra([freq, freq, freq], [psd_lfp, psd_e, psd_i],
+    plot_power_spectra([freq, freq, freq], [psd, psd_e, psd_i],
                     ['LFP', 'Excitatory', 'Inhibitory'])
     plt.savefig('1D.png')
     
@@ -79,7 +84,8 @@ def simulate_lfp():
 def corr_EIRatio_and_slope():
     # analysis
     PSDs, freq = batchsim_PSDs(EI_ratios=EI_RATIO, num_trs=N_SIMS)
-    slopes = batchfit_PSDs(PSDs, freq, EI_ratios = EI_RATIO, num_trs=N_SIMS, freq_range=F_RANGE_FIT)
+    slopes = batchfit_PSDs(PSDs, freq, EI_ratios = EI_RATIO, num_trs=N_SIMS, 
+                           freq_range=F_RANGE_FIT)
     
     # Figure 1, E.
     # Plot power spectra for two different E:I ratios
@@ -92,36 +98,43 @@ def corr_EIRatio_and_slope():
     plt.close('all')
     
     # Figure 1, F.
-    df = pd.DataFrame(slopes, columns = ['Trial1','Trial2','Trial3','Trial4','Trial5'])
+    # Plot Figure 1, F.
+    df = pd.DataFrame(slopes, 
+                      columns=['Trial1','Trial2','Trial3','Trial4','Trial5'])
     df['EIRatio'] = 1./EI_RATIO
     df_plot = df.melt('EIRatio')
-    ax = sns.lineplot(data=df_plot, x = 'EIRatio', y = 'value', marker='o', color = 'black')
-    ax.set_xscale('log', base=2)
+    ax = sns.lineplot(data=df_plot, x='EIRatio', y='value', marker='o', 
+                      color='black')
     ax.set_xlabel('g_E : g_I Ratio')
     ax.set_ylabel('Slope (30-50Hz)')
     ax.set_title('EI Ratio, PSD Slope Correlation Plot')
-    fig4 = ax.get_figure()
-    fig4.savefig('1F.png')
-    ax.clear()
+    fig_1f = ax.get_figure()
+    fig_1f.savefig('1F.png')
+    plt.close('all')
     
     return PSDs, freq
  
     
 def assess_corr_across_freqs(PSDs, freq):
-    rhos = batchcorr_PSDs(PSDs, freq, EI_ratios = EI_RATIO, center_freqs=F_RANGE_CENTER, 
+    
+    # Correlate EI ratio and slope fit in different frequency ranges
+    rhos = batchcorr_PSDs(PSDs, freq, EI_ratios = EI_RATIO, 
+                          center_freqs=F_RANGE_CENTER, 
                win_len=F_RANGE_WIDTH, num_trs=N_SIMS)  
      
-    # Figure 1, G.
-    df2 = pd.DataFrame(rhos, columns = ['Trial1','Trial2','Trial3','Trial4','Trial5'])
+    # Plot Figure 1, G.    
+    df2 = pd.DataFrame(rhos, 
+                       columns=['Trial1','Trial2','Trial3','Trial4','Trial5'])
     df2['CenterFreq'] = F_RANGE_CENTER
     df2_plot = df2.melt('CenterFreq')
-    ax2 = sns.lineplot(data=df2_plot, x = 'CenterFreq', y = 'value', marker='o', color = 'black')
+    ax2 = sns.lineplot(data=df2_plot, x='CenterFreq', y='value', marker='o', 
+                       color='black')
     ax2.set_xlabel('Fit Center Frequency (+-10 Hz)')
     ax2.set_ylabel('Spearman Correlation')
     ax2.set_title('Spearman Correlation - Fitting Window Plot')
-    fig5 = ax2.get_figure()
-    fig5.savefig('1G.png')
-
+    fig_1g = ax2.get_figure()
+    fig_1g.savefig('1G.png')
+    plt.close('all')
 
 if __name__ == "__main__":
     main()
