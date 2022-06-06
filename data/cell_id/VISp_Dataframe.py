3f9a5fb03ad442ac01d2988e1c29acc8a3f68ed9
SM-Figueroa <s2figuer@ucsd.edu>
2022-06-05 20:06:36 -0700

    DataFrame including all VISp units exported to csv

---
+++ data/cell_id/VISp_Dataframe.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import pandas as pd
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+
+#Define data directory and create Allensdk cache object
+data_directory = '/Users/User/Documents/Voytek Lab/ManifestFiles'
+manifest_path = os.path.join(data_directory, "manifest.json")
+cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+
+#Get all units and query for VISp units
+units = cache.get_units()
+meta_total=units.reset_index().rename(columns={'id':'unit_id'})
+V1_meta=meta_total[meta_total.get('ecephys_structure_acronym')=='VISp'].get(['unit_id','ecephys_session_id','ecephys_probe_id','specimen_id']).drop_duplicates()
+
+#Convert DataFrame to csv
+V1_meta.to_csv('V1_meta',index=False)
