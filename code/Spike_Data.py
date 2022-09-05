c7f99ba16fc6a88ffe0a10f5e1e3bfaaf15e61e9
SM-Figueroa <s2figuer@ucsd.edu>
2022-09-04 21:08:36 -0700

    refactor: import allen_vc function

--- code/Spike_Data.py
+++ code/Spike_Data.py
@@ -1,15 +1,14 @@
 # imports
 import os
-import numpy as np
 import pandas as pd
 import pickle
-from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+from allen_vc.utils import get_spiking_data
 
 #NOTE: Must run Brain_Structure_DataFrame for given brain_structure beforehand
 
 # settings
-PROJECT_PATH = 'C:\\Users\\User\\visual_encoding'
-BRAIN_STRUCTURE = 'VISp'
+PROJECT_PATH = 'C:\\Users\\User\\visual_encoding' #'C:/users/micha/visual_encoding'
+BRAIN_STRUCTURES = ['VISp','LGd']
 
 def main():
 
@@ -20,32 +19,22 @@ def main():
     
     # Create Allensdk cache object
     manifest_path = f"{PROJECT_PATH}/data/manifest_files/manifest.json"
-    cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
 
-    meta=pd.read_csv(f'{PROJECT_PATH}\\data\\brain_structure_DataFrames\\{BRAIN_STRUCTURE}_DataFrame.csv')
+    for structure in BRAIN_STRUCTURES:
+        meta=pd.read_csv(f'{PROJECT_PATH}\\data\\brain_structure_DataFrames\\{structure}_DataFrame.csv')
 
-    for ses in meta.get('ecephys_session_id').unique():
-        #Retrieve data and store in dictionaries
-        spike_times={}
-        spike_amplitudes={}
-        mean_waveforms={}
+        for session_id in meta.get('ecephys_session_id').unique():
+            print(f'Analyzing session: \t{session_id}')
+            spike_times, spike_amplitudes, mean_waveforms = get_spiking_data(session_id, manifest_path, structure)
 
-        session = cache.get_session_data(ses, isi_violations_maximum = np.inf,
-                                amplitude_cutoff_maximum = np.inf,
-                                presence_ratio_minimum = -np.inf)
-        for unit in meta[meta.get('ecephys_session_id')==ses].get('unit_id').unique():
-            spike_times[unit]=session.spike_times[unit]
-            spike_amplitudes[unit]=session.spike_amplitudes[unit]
-            mean_waveforms[unit]=session.mean_waveforms[unit]
-
-        # create a binary pickle file 
-        for variable, var_str in zip([spike_times,spike_amplitudes,mean_waveforms],
-                                      ['spike_times', 'spike_amplitudes',
-                                        'mean_waveforms']):
-            fname_out = f"{dir_results}/{str(ses)}_{BRAIN_STRUCTURE}_{var_str}.pkl"
-            h = open(fname_out,"wb")
-            pickle.dump(variable, h)
-            h.close()
+            # create a binary pickle file 
+            for variable, var_str in zip([spike_times,spike_amplitudes,mean_waveforms],
+                                          ['spike_times', 'spike_amplitudes',
+                                            'mean_waveforms']):
+                fname_out = f"{dir_results}/{str(session_id)}_{structure}_{var_str}.pkl"
+                h = open(fname_out,"wb")
+                pickle.dump(variable, h)
+                h.close()
 
 
 if __name__ == '__main__':
