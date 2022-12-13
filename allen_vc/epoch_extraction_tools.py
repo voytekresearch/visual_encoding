fe4b75b6985a4d831d383a0a6e49e06ec6492293
Michael <mpreston@ucsd.edu>
2022-12-13 10:48:40 -0800

    create utility function for plotting

--- allen_vc/epoch_extraction_tools.py
+++ allen_vc/epoch_extraction_tools.py
@@ -109,45 +109,6 @@ def find_segments(signal, threshold, return_below=False):
         return epoch_times
 
 
-def plot_epochs(signal, time, epochs, threshold=None):
-    """Plots a signal over time, with annotations for epochs.
-
-    Parameters
-    ----------
-    signal : numpy array
-        Signal to be plotted.
-    time : numpy array
-        Time stamps for the signal.
-    epochs : 2D numpy array
-        Epochs to annotate.
-    threshold : float, optional
-        Horizontal line at given value.
-
-    Returns
-    -------
-    fig, ax : matplotlib Figure, Axes
-        Figure and axes for the plot.
-    """
-    # imports
-    import matplotlib.pyplot as plt
-
-    # plot signal
-    fig, ax = plt.subplots(figsize=[20,4])
-    ax.plot(time, signal)
-
-    # annotate threshold
-    if threshold is not None:
-        ax.axhline(threshold, color='k')
-
-    # annotate epochs
-    for t_start in np.array(time[epochs[:,0]]):
-        ax.axvline(t_start, color='b')
-    for t_stop in np.array(time[epochs[:,1]]):
-        ax.axvline(t_stop, color='r')
-
-    return fig, ax
-
-
 def join_epochs_with_gap(epochs, min_gap):
     """
     Joins together epochs that have a gap shorter than a given minimum duration between them.
