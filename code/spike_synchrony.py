2924ab24a21b5156cd98a2e052bfd5f076112136
SM-Figueroa <s2figuer@ucsd.edu>
2023-01-29 17:49:02 -0800

    fix: adapt spike_synchrony.py to handle neo blocks

--- code/spike_synchrony.py
+++ code/spike_synchrony.py
@@ -16,12 +16,7 @@ REPO_PATH = 'C:/Users/User/visual_encoding' # code repo r'C:\Users\micha\visual_
 
 # settings - data of interest
 BRAIN_STRUCTURES = ['VISp', 'LGd']
-STIMULUS_NAME = 'natural_movie_one_more_repeats'
-# BLOCK = 1
-EPOCH_LENGTH = 30
-STATES = range(60)
-EPOCH_FILEPATH = f'{PROJECT_PATH}/data/behavior/running/epoch_times/'+\
-        'natural_movie_one_times.npz'
+STIMULUS_NAME = "natural_movie_one_more_repeats"
 
 # Import custom functions
 import sys
@@ -30,51 +25,49 @@ from allen_vc.analysis import calculate_spike_metrics
 
 def main():
     #Initialize space for data storage
-    data = [0]*9
-
-    epochs = np.load(EPOCH_FILEPATH)
+    
+    data_mat = [0]*9
 
     for structure in BRAIN_STRUCTURES:
+        print(f"\nAnalyzing Region:\t{structure}")
         meta=pd.read_csv(f'{PROJECT_PATH}\\data\\brain_structure_DataFrames\\{structure}_DataFrame.csv')
 
         # Iterate over each session
         for session_id in meta.get('ecephys_session_id').unique():
+            print(f"\n\tAnalyzing Session:\t{session_id}")
+
             # skip over weird session
-            if STIMULUS_NAME == 'natural_movie_one_more_repeats' and \
+            if (STIMULUS_NAME == 'natural_movie_one_more_repeats' or \
+                STIMULUS_NAME == 'natural_movie_one_more_repeats_all') and \
             session_id==793224716:
                 continue
 
-            # load Neo SpikeTrains
-            spiketrains = pd.read_pickle(f'{PROJECT_PATH}/data/spike_data'+\
-                f'/spike_times/{str(session_id)}_{structure}.pkl')
+            # not all sessions have been loaded yet so put this in for safety
+            if not os.path.exists(f"{PROJECT_PATH}/data/blocks/{session_id}_{STIMULUS_NAME}.pkl"):
+                continue
+
+            block = pd.read_pickle(f"{PROJECT_PATH}/data/blocks/{session_id}_{STIMULUS_NAME}.pkl")
 
             # Calculate spike metrics for both states
-            for epoch_type in STATES:
-                epoch = epochs[f'{session_id}_{epoch_type}']
+            for segment in block.segments:
 
-                if len(epoch)>0:
-                    # epoch spiking data for state
-                    spiketrains_state = [spiketrain.time_slice(epoch[0]*pq.s, epoch[1]*pq.s) \
-                    for spiketrain in spiketrains]
+                spikes = list(segment.filter(region=structure))
 
-                    # Calculate/combine metrics for storage
-                    metrics = list(calculate_spike_metrics(spiketrains_state))
-                    metrics += [epoch, epoch_type, structure, session_id]
-                    data = np.vstack((data, metrics))
-                else:
-                    metrics = [None]*6 + [epoch_type, structure, session_id]
-                    data = np.vstack((data, metrics))
+                # Calculate/combine metrics for storage
+                metrics = list(calculate_spike_metrics(spikes))
+                metrics += [list((segment.t_start, segment.t_stop)), segment.name, structure, session_id]
+                data_mat = np.vstack((data_mat, metrics))
 
     #Save DataFrame including all metrics
-    data = np.delete(data, (0), axis=0)
+    data_mat = np.delete(data_mat, (0), axis=0)
     labels = ['mean_firing_rate', 'coefficient_of_variation', \
     'SPIKE-distance','SPIKE-synchrony','correlation coefficient', \
     'epoch', 'state', 'brain_structure','session_id']
-    
+
     for base_path in [PROJECT_PATH, MANIFEST_PATH]:
-        pd.DataFrame(data=data, columns=labels).\
-        to_csv(f'{PROJECT_PATH}/data/spike_data/synchrony_data/{BRAIN_STRUCTURES}_'+\
-            f'{STIMULUS_NAME}_{EPOCH_LENGTH}s.csv', index=False)
+        fname_out = "-".join(BRAIN_STRUCTURES) + "_" + STIMULUS_NAME
+        pd.DataFrame(data=data_mat, columns=labels).to_csv(f'{PROJECT_PATH}/data/spike_data'+\
+            f'/synchrony_data/{fname_out}.csv', index=False)
 
 if __name__ == '__main__':
     main()
