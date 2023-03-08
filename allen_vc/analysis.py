cbe17552fdf649668e0684e72d04d397012f763d
SM-Figueroa <s2figuer@ucsd.edu>
2023-03-07 16:55:25 -0800

    feat: add functions from movie_comparisons.ipynb

--- allen_vc/analysis.py
+++ allen_vc/analysis.py
@@ -76,3 +76,190 @@ def calculate_spike_metrics(spiketrains):
 
     return mean_firing_rate, unit_firing_rates, coeff_of_var, spike_dist, spike_sync, corr_coeff
 
+
+
+# take the median over all channels for each epoch
+def channel_medians(lfp_df, col_names):
+    #imports
+    import pandas as pd
+    
+    medians = [0]*len(col_names)
+    for epoch in lfp_df.get("epoch_idx").unique():
+        epoch_df = lfp_df[lfp_df.get("epoch_idx")==epoch]
+        medians = np.vstack((medians, epoch_df.median()))
+
+    medians = np.delete(medians, (0), axis=0)
+    return pd.DataFrame(data = medians, columns = col_names)#.drop(columns = 'chan_idx')
+
+
+def avg_psd_over_freq_ranges(freq, psd, lower_lims, upper_lims, log_transform=False):
+    """
+    Compute the average power spectral density (PSD) over frequency ranges for a given set of trials.
+
+    Parameters
+    ----------
+    freq : array_like
+        Array of frequencies.
+    psd : array_like
+        Array of PSD values.
+    lower_lims: array_like
+        Array of lower limits for frequency ranges.
+    upper_lims: array_like
+        Array of upper limits for frequency ranges.
+    log_transform : bool, optional
+        Whether to take the log10 of the PSD values before computing the average. Default is False.
+
+    Returns
+    -------
+    mat : array_like
+        3D array of average PSD values over frequency ranges for each trial.
+    """
+    # determine number of trials in block
+    num_trials = psd.shape[1]
+    
+    # intialize empty matrix for storage
+    mat = np.empty((30, 30, 0))
+    
+    # loop through trials
+    for trial in range(num_trials):
+        
+        # take median across channels
+        psd_trial = np.median(psd[:,trial,:], axis=0)
+        trial_mat = []
+        
+        # loop through upper/lower frequency limits
+        for upper_lim in upper_lims:
+            row = []
+            for lower_lim in lower_lims:
+                if upper_lim<=lower_lim:
+                    row.append([np.nan])
+                    continue
+                    
+                # filter for range and compute average
+                psd_range = psd_trial[(freq>lower_lim) & (freq<=upper_lim)]
+                if log_transform:
+                    psd_avg = np.mean(np.log10(psd_range))
+                else:
+                    psd_avg = np.mean(psd_range)
+                row.append([psd_avg])
+            trial_mat.append(row)
+            
+        # stack matrices for each trial
+        mat = np.dstack((mat, trial_mat))
+        
+    return mat
+
+
+def create_r_matrix(statistics, mat):    
+    """
+    Create a matrix of Pearson correlation coefficients between spike statistics and PSD data.
+
+    Parameters
+    ----------
+    statistics : array_like
+        Array of spike statistics.
+    mat : array_like
+        3D array of PSD data.
+
+    Returns
+    -------
+    r_mat : array_like
+        2D array of Pearson correlation coefficients.
+    """
+    # initialize matrix for storage of r values
+    r_mat = []
+    p_mat = []
+    
+    # loop through data in mat
+    for i, row in enumerate(mat):
+        r_mat_row = []
+        p_mat_row = []
+        for j, col in enumerate(row):
+            all_trials = mat[i,j,:]
+            
+            # disregard invalid ranges
+            if all(np.isnan(all_trials)):
+                results = [np.nan, np.nan]
+            else:
+                # some trials have NaN PSD data so filter those out
+                results = sts.pearsonr(statistics[~np.isnan(all_trials)], \
+                                      all_trials[~np.isnan(all_trials)])
+            r_mat_row.append(results[0])
+            p_mat_row.append(results[1])
+        r_mat.append(r_mat_row)
+        p_mat.append(p_mat_row)
+        
+    return np.array(r_mat), np.array(p_mat)
+
+# combine full pipeline
+def sensitivity_analysis(metric, region, psd_file_path, spikes_file_path, plot_sessions=True,
+                         show_sessions=False, mask_nonsig=False, dir_figures=None):
+    
+    movie_synchrony_df = pd.read_csv(spikes_file_path)
+    
+    # filter for v1 but later do LGd
+    region_movie_synchrony_df = movie_synchrony_df[movie_synchrony_df.get('brain_structure')==region]
+
+    # initialize matrix to hold all session data
+    all_sessions_r = np.empty((30, 30, 0))
+    all_sessions_p = np.empty((30, 30, 0))
+
+    # loop through all sessions/files
+    for file in os.listdir(psd_file_path):
+
+        # skip weird session with abnormal trial times/counts
+        ses_id = int(file.split('_')[0])
+        if ses_id == 793224716 or ses_id not in region_movie_synchrony_df.get('session_id').unique():
+            continue
+
+        # log progress
+        # print(f"Analyzing File:\t{file}")
+
+        # load data
+        data_in = np.load(f'{psd_file_path}/{file}')
+        psd, freq = data_in['psd'], data_in['freq']
+
+        # calculate average psd over frequency ranges
+        upper_lims = range(300, 0, -10)
+        lower_lims = range(0, 300, 10)
+        # use log transformation before average PSD values over range
+        freq_range_mat = avg_psd_over_freq_ranges(freq, psd, lower_lims, upper_lims, log_transform=True)
+
+        # filter by session and get metric
+        ses_id = int(file.split('_')[0])
+        ses_df = region_movie_synchrony_df[region_movie_synchrony_df.get('session_id')==ses_id]
+        spike_stats = ses_df.get(metric)
+
+        # compute pearson R between average PSDs and spike metric values for each trial
+        r_mat, p_mat = create_r_matrix(spike_stats, freq_range_mat)
+        sig_mat = p_mat < 0.05 # determine significance
+        
+        # set fname_out if saving figures
+        if not dir_figures is None:
+            fname_out = f'{dir_figures}/{region}_{metric}_{str(ses_id)}.png'
+        else:
+            fname_out = None
+
+        # plot heat map of r values
+        if plot_sessions:
+            if mask_nonsig:
+                plot_sa_heat_map(r_mat, lower_lims, upper_lims, f'{str(ses_id)} - {region} {metric}',
+                             sig_mask=sig_mat, fname_out=fname_out, show_fig=show_sessions)
+            else:
+                plot_sa_heat_map(r_mat, lower_lims, upper_lims, f'{str(ses_id)} - {region} {metric}',
+                                 fname_out=fname_out, show_fig=show_sessions)
+
+        # aggregate all sessions
+        all_sessions_r = np.dstack((all_sessions_r, r_mat))
+    
+    # plot session average
+    ses_avg_r_mat = np.mean(all_sessions_r, axis=2)
+    if dir_figures:
+        plot_sa_heat_map(ses_avg_r_mat, lower_lims, upper_lims, f"Session Average - {region} {metric}",
+                         fname_out=f'{dir_figures}/{region}_session_avg_{metric}.png')
+    else:
+        plot_sa_heat_map(ses_avg_r_mat, lower_lims, upper_lims, f"Session Average - {region} {metric}")
+        
+     # save out all_sessions matrix
+    np.save(f"{MATRICES_OUT}/{psd_file_path.split('/')[-1]}/{region}_{metric}", all_sessions_r)
+
--- allen_vc/plts.py
+++ allen_vc/plts.py
@@ -76,3 +76,114 @@ def sync_plot(df, metrics, condition):
         plt.title(f'{metric}')
         vp = sns.violinplot(**plotting_params, ax=ax, color = 'magenta')
         sp = sns.swarmplot(**plotting_params, ax=ax, color=[0,0,0])
+
+# compare linear regression plots between movie and shuffled conditions for two variables
+def linregress_movie_v_shuffled_plot(x1, y1, x2=None, y2=None, title=None, 
+                                     xlabel=None, ylabel=None, fname_out=None, show=False):
+    import scipy.stats as sts
+    
+    #natural_movie
+    m1 = sts.linregress(x1, y1)
+    l1 = np.linspace(min(x1), max(x1), 1000)
+    t1 = m1.slope * l1 + m1.intercept
+    
+    #shuffled
+    m2 = sts.linregress(x2, y2)
+    l2 = np.linspace(min(x2), max(x2), 1000)
+    t2 = m2.slope * l2 + m2.intercept
+    
+    plt.style.use('bmh')
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.add_subplot()
+    
+    plt.scatter(x1, y1)
+    plt.plot(l1, t1, label='natural_movie (1)')
+    plt.scatter(x2, y2)
+    plt.plot(l2, t2, label='shuffled (2)')
+    plt.legend()
+    
+    # add labels
+    plt.text(1.05, 0.95, f"Slope1: {round(m1.slope, 5)}", transform = ax.transAxes)
+    plt.text(1.05, 0.9, f"R1: {round(m1.rvalue, 5)}", transform = ax.transAxes)
+    plt.text(1.05, 0.85, f"p: {round(m1.pvalue, 5)}", transform = ax.transAxes)
+    plt.text(1.05, 0.8, f"Intercept1: {round(m1.intercept, 5)}", transform = ax.transAxes)
+    
+    plt.text(1.05, 0.7, f"Slope2: {round(m2.slope, 5)}", transform = ax.transAxes)
+    plt.text(1.05, 0.65, f"R2: {round(m2.rvalue, 5)}", transform = ax.transAxes)
+    plt.text(1.05, 0.6, f"p: {round(m2.pvalue, 5)}", transform = ax.transAxes)
+    plt.text(1.05, 0.55, f"Intercept2: {round(m2.intercept, 5)}", transform = ax.transAxes)
+    
+    if title:
+        plt.title(title)
+    if xlabel:
+        plt.xlabel(xlabel)
+    if ylabel:
+        plt.ylabel(ylabel)
+        
+    if not fname_out is None:
+        plt.savefig(fname_out)
+        
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+def plot_sa_heat_map(r_mat, xlabels, ylabels, graph_title=None,
+                     sig_mask=None, fname_out=None, show_fig=True, vmin=-1, vmax=1):
+    """
+    Plot a sensitivity analysis heat map with -1 to 1 color map limits.
+
+    Parameters
+    ----------
+    r_mat : array_like
+        2D array of sensitivity analysis results.
+    sig_mat : array_like
+        2D array of bool values indicating whether a result is significant.
+    xlabels: array_like
+        labels for x axis
+    ylabels: array_like
+        labels for y axis
+    graph_title (optional): str
+        title of heat map created
+    sig_mask (optional): array_like
+        2D array of bool values indicating whether a result is significant.
+        Default is None. If None, all values are plotted.
+    fname_out (optional): str
+        path to save figure. Default is None. If None, figure is not saved.
+    show_fig (optional): bool
+        whether to show figure. Default is True.
+
+    Returns
+    -------
+    None
+
+    """
+
+    # set non-significant values to 0
+    if not sig_mask is None:
+        data_plt = np.zeros_like(r_mat)
+        data_plt[sig_mask] = r_mat[sig_mask]
+    else:
+        data_plt = r_mat
+
+    # plot
+    plt.figure(figsize=(14,7))
+    sns.heatmap(data_plt, xticklabels=xlabels, yticklabels=ylabels, 
+                vmin=vmin, vmax=vmax, center=0, cbar_kws={'label': 'Pearson R'}, cmap='coolwarm')
+    
+    # label
+    plt.xlabel("Lower Limit (Hz)")
+    plt.ylabel("Upper Limit (Hz)")
+    plt.xticks(rotation=30)
+    if graph_title:
+        plt.title(graph_title)
+
+    # save
+    if not fname_out is None:
+        plt.savefig(fname_out)
+
+    if show_fig:
+        plt.show()
+    else:
+        plt.close()
+
