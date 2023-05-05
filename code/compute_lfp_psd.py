4f88d119f1d232a1c4cf556d9e2eda8b46105360
Michael <mpreston@ucsd.edu>
2023-05-04 22:00:52 -0700

    feat: implement compute_tfr()

--- code/compute_lfp_psd.py
+++ code/compute_lfp_psd.py
@@ -6,26 +6,30 @@ Compute PSD for LFP epochs. Analyzes input of allen_vc.epoch_lfp.py.
 PROJECT_PATH = "G:/Shared drives/visual_encoding" # shared results directory
 STIM_CODE = 'natural_movie_one_more_repeats' # name of input/output folders (stimulus of interest)
 
+# settings - analysis details
+N_JOBS = -1 # number of jobs to run in parallel
+F_MIN = 2 # min freq for TF decomposition
+F_MAX = 200 # max freq for TF decomposition
+N_FREQS = 128 # number of freqs for TF decomposition
+DECIM = 25 # decimation factor for TF decomposition
+
+# settings - dataset details
+FS = 1250 # LFP sampling freq
+
 # imports
 import os
 import numpy as np
 from time import time as timer
 import neo
-from mne.time_frequency import psd_array_multitaper
+from mne import create_info, EpochsArray
 
 # imports - custom
 import sys
 sys.path.append("allen_vc")
 from utils import hour_min_sec
 from neo_utils import get_analogsignal
+from analysis import compute_tfr
 print('Imports complete...')
-
-# settings - analysis details
-N_JOBS = 8 # number of jobs to run in parallel for psd_array_multitaper()
-F_RANGE = [1, 300]
-
-# settings - dataset details
-FS = 1250 # LFP sampling freq
 
 def main():
     # time it
@@ -43,20 +47,23 @@ def main():
         
         # display progress
         t_start_s = timer()
-        print(f"\nAnalyzing file {i_file+1}/{len(files)}")
-        print(f"    {fname_in}")
+        print(f"\nAnalyzing {fname_in} ({i_file+1}/{len(files)})")
 
         # load block and extract lfp
         block = neo.io.NeoMatlabIO(f"{dir_input}/{fname_in}").read_block()
-        lfp = get_analogsignal(block, 'lfp')
+        lfp, annotations = get_analogsignal(block, 'lfp', return_annotations=True)
+        ch_names = annotations['channel_ids'].astype(str).tolist()
 
-        # compute psd
-        psd, freq = psd_array_multitaper(lfp, FS, fmin=F_RANGE[0], 
-                                         fmax=F_RANGE[1], n_jobs=N_JOBS)
-
+        # create mne epochs array and compute power using multitapers
+        info = create_info(ch_names=ch_names, sfreq=FS, ch_types='eeg')
+        epochs = EpochsArray(lfp, info, tmin=0, verbose=False)
+        time, freq, tfr = compute_tfr(epochs, f_min=F_MIN, f_max=F_MAX, 
+                                      n_freqs=N_FREQS, decim=DECIM, 
+                                      n_jobs=N_JOBS, verbose=False)
+        
         # save results
         fname_out = fname_in.replace('.mat', '.npz')
-        np.savez(f"{dir_results}/{fname_out}", psd=psd, freq=freq) 
+        np.savez(f"{dir_results}/{fname_out}", tfr=tfr, time=time, freq=freq) 
 
         # display progress
         hour, min, sec = hour_min_sec(timer() - t_start_s)
