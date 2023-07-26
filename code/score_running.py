fe22e05182d7a278349c2bba445c6a298b600afa
Michael <mpreston@ucsd.edu>
2023-07-26 12:49:06 -0700

    feat: add multi-path option

--- code/score_running.py
+++ code/score_running.py
@@ -1,28 +1,32 @@
 """
-This script loads a segmented Neo Block and scores the running behavior. This can be used to add
-behavioral data to the outpt of step2_create_stimulus_segments.py if this info was not added during
-the initial segmentation.
+This script loads a segmented Neo Block and scores the running behavior. This is performed for all files 
+in a given folder, or list of folders. This can be used to add behavioral data to the output of 
+step2_create_stimulus_segments.py if this info was not added during the initial segmentation.
 """
 
-# SET PATH
-PATH_IN = r"G:\Shared drives\visual_encoding\data\blocks\segmented\natural_movie_one_more_repeats"
+# SET PATH 
+PATHS = [r"G:\Shared drives\visual_encoding\data\blocks\segmented\natural_movie_one_shuffled",
+           r"G:\Shared drives\visual_encoding\data\blocks\lfp\natural_movie_one_more_repeats",
+           r"G:\Shared drives\visual_encoding\data\blocks\lfp\natural_movie_one_shuffled"]
 
 # imports
 import os
 import neo
 from step2_create_stimulus_segments import score_running
 
-# loop through files
-files = os.listdir(PATH_IN)
-for i_file, fname in enumerate(files):
-    # display progress
-    print(f"\nAnalyzing: {fname} ({i_file+1}/{len(files)})")
+# loop through folders
+for path in PATHS:
+    # loop through files
+    files = os.listdir(path)
+    for i_file, fname in enumerate(files):
+        # display progress
+        print(f"\nAnalyzing: {fname} ({i_file+1}/{len(files)})")
 
-    # load block
-    block = neo.io.NeoMatlabIO(filename=f"{PATH_IN}/{fname}").read_block() 
+        # load block
+        block = neo.io.NeoMatlabIO(filename=f"{path}/{fname}").read_block() 
 
-    # score running
-    block = score_running(block)
+        # score running
+        block = score_running(block)
 
-    # save block
-    neo.io.NeoMatlabIO(filename=f"{PATH_IN}/{fname}").write_block(block)
+        # save block
+        neo.io.NeoMatlabIO(filename=f"{path}/{fname}").write_block(block)
