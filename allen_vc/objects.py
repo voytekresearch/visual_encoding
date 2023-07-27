5ef1c79c30dc3a39f999415eee42a04ac5d4ba0a
Michael <mpreston@ucsd.edu>
2023-07-27 13:49:31 -0700

    feat: add print statements

--- allen_vc/objects.py
+++ allen_vc/objects.py
@@ -13,52 +13,65 @@ import neo
 # define class
 class SessionResults:
     def __init__(self, path, session_id, stim_code, load_psd=False, load_tfr=False):
+        # display progress
+        print(f"\nLoading results for session {session_id}:")
+
         # save inputs
         self.path = path
         self.session_id = session_id
         self.stim_code = stim_code
 
         # load psd results
-        try:
-            self.psd_params = pd.read_csv(f"{path}/data/lfp_data/lfp_params/{stim_code}/psd/by_session/params_{session_id}.csv")
-        except:
-            self.psd_params = None
-            print(f"PSD spectral parameters not found for session {session_id}")
         if load_psd:
             self.load_psd()
         
-        # load tfr results
         try:
-            self.tfr_params = pd.read_csv(f"{path}/data/lfp_data/lfp_params/{stim_code}/tfr/by_session/params_{session_id}.csv")
+            self.psd_params = pd.read_csv(f"{path}/data/lfp_data/lfp_params/{stim_code}/psd/by_session/params_{session_id}.csv")
+            print(f"    PSD spectral parameters loaded...")
         except:
-            self.tfr_params = None
-            print(f"TFR spectral parameters not found for session {session_id}")
+            self.psd_params = None
+            print(f"    PSD spectral parameters not found...")
+
+        # load tfr results
         if load_tfr:
             self.load_tfr()
+
+        try:
+            self.tfr_params = pd.read_csv(f"{path}/data/lfp_data/lfp_params/{stim_code}/tfr/by_session/params_{session_id}.csv")
+            print(f"    TFR spectral parameters loaded...")
+        except:
+            self.tfr_params = None
+            print(f"    TFR spectral parameters not found...")
         
         # load spike results
         try:
             all_spike_stats = pd.read_csv(f"{path}/data/spike_stats/{stim_code}.csv")
             self.spike_stats = all_spike_stats.loc[all_spike_stats['session'] == session_id]
+            print(f"    Spike stats loaded...")
         except:
             self.spike_stats = None
-            print(f"Spike stats not found for session {session_id} and stimulus {stim_code}")
+            print(f"    Spike stats not found...")
+
+        # print summary
+        print("    Complete!\n")
 
     def load_tfr(self):
         try:
             data_in = np.load(f"{self.path}/data/lfp_data/lfp_tfr/{self.stim_code}/spectra_{self.session_id}.npz")
             self.tfr = data_in['tfr']
             self.tfr_freq = data_in['freq']
+            print(f"    TFR data loaded...")
         except:
-            print(f"TFR data not found for session {self.session_id}")
+            print(f"TFR data not found...")
 
     def load_psd(self):
         try:
             data_in = np.load(f"{self.path}/data/lfp_data/lfp_psd/{self.stim_code}/spectra_{self.session_id}.npz")
             self.psd = data_in['spectra']
             self.psd_freq = data_in['freq']
+            print(f"    PSD data loaded...")
         except:
-            print(f"PSD data not found for session {self.session_id}")
+            print(f"PSD data not found...")
 
     def import_block(self, include_lfp=False):
         if include_lfp:
